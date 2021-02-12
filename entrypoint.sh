b3f44318f2e81f1b584357553c39aa5a1a34ca93
John Posten <john.posten@gmail.com>
2021-02-11 17:15:13 -0700

    echo output

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 #sed -i -e 's/name \"My server\"'"name \"${SERVER_NAME}\""'/g' "${STEAMAPPDIR}/start_server.sh"
-
+echo Change directory to ${STEAMAPPDIR} and Starting server...
 cd "${STEAMAPPDIR}" && bash "${STEAMAPPDIR}/start_server.sh"
