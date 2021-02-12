4c7a012ec746b351f0f5c090637c7e96c6a0cd58
John Posten <john.posten@gmail.com>
2021-02-11 17:26:38 -0700

    dont validate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
-				+app_update "${STEAMAPPID}" validate \
+				+app_update "${STEAMAPPID}" \
 				+quit
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 #sed -i -e 's/name \"My server\"'"name \"${SERVER_NAME}\""'/g' "${STEAMAPPDIR}/start_server.sh"
-echo Change directory to ${STEAMAPPDIR} and Starting server...
+echo "Change directory to ${STEAMAPPDIR} and Starting server..."
+
 cd "${STEAMAPPDIR}" && bash "${STEAMAPPDIR}/start_server.sh"
